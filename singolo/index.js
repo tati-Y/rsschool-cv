841b49e0ffd4d5c024bc0faddfd2580b226aeb41
Tania Yufanova <ufanchik@gmail.com>
2021-01-08 01:47:32 +0300

    feat: add simple slider without flipping

--- singolo/index.js
+++ singolo/index.js
@@ -48,3 +48,23 @@ const replacePortfolio = () => {
   console.log('ku');
 }
 
+//simple slider
+
+let images = document.querySelectorAll('.slider_image img');
+let current = 0;
+
+function slider() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add('opacity0');
+  }
+  images[current].classList.remove('opacity0');
+  
+  if (current+1 === images.length) {
+    current = 0;
+  } else {
+    current++;
+  }
+}
+
+document.querySelector('.container__arrow_left').onclick = slider;
+document.querySelector('.container__arrow_right').onclick = slider;
