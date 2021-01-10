0ae98bb30bc5b4fe85ad9e711f72dd7ae38e1e4b
Tania Yufanova <ufanchik@gmail.com>
2021-01-10 22:48:07 +0300

    feat: add slider with flipping

--- singolo/index.js
+++ singolo/index.js
@@ -46,7 +46,7 @@ const replacePortfolio = () => {
 
   let shuffledArray = picsArray.sort((a,b) => 0.5 - Math.random());
 
-  for (let i = 0; i <shuffledArray.length; i++) {
+  for (let i = 0; i < shuffledArray.length; i++) {
     let img = document.createElement('img');
     img.src = shuffledArray[i];
     img.classList.add('pic');
@@ -56,9 +56,9 @@ const replacePortfolio = () => {
 
 document.querySelector('.portfolio__tags').onclick = replacePortfolio;
 
-//simple slider
+//simple slider (without flipping)
 
-let images = document.querySelectorAll('.slider_image img');
+/*let images = document.querySelectorAll('.slider_image img');
 let current = 0;
 
 function slider() {
@@ -75,7 +75,90 @@ function slider() {
 }
 
 document.querySelector('.container__arrow_left').onclick = slider;
-document.querySelector('.container__arrow_right').onclick = slider;
+document.querySelector('.container__arrow_right').onclick = slider;*/
+
+//slider with flipping
+
+let slides = document.querySelectorAll('.slide');
+  let slider = [];
+  for (let i = 0; i <slides.length; i++) {
+    slider[i] = slides[i].src;
+    slides[i].remove();
+  }
+  
+
+  let step = 0;
+  let offset = 0; 
+
+  
+   function drawVisibleSlideToLeft() {
+        let slide = document.createElement('img');
+        slide.src = slider[step];
+        slide.classList.add('slide');
+        slide.style.left = offset*1020 + 'px';
+        document.querySelector('.slider_image').appendChild(slide); 
+        if (step+1 === slider.length) {
+          step = 0;
+        } else {
+          step++;}
+        //step++;
+        offset = 1;               
+    }
+    drawVisibleSlideToLeft(); 
+
+  function left() {
+    drawVisibleSlideToLeft();
+    let slides2 = document.querySelectorAll('.slide');
+    let offset2 = 0;
+
+    for (let i = 0; i < slides2.length; i++) {
+      slides2[i].style.left = offset2*1020 - 1020 +'px';
+      offset2++;
+    }
+
+    setTimeout(function() {
+      slides2[0].remove();
+      draw();
+      drawVisibleSlideToLeft();
+    }, 1000);
+}
+
+  
+function drawVisibleSlideToRight() {    
+      let slide = document.createElement('img');
+      slide.src = slider[step];
+      slide.classList.add('slide');
+      slide.style.left = offset*1020 + 'px';
+      document.querySelector('.slider_image').appendChild(slide); 
+      if (step+1 === slider.length) {
+        step = 0;
+      } else {
+        step++;}
+      //step++;
+      offset = -1;
+  }
+  drawVisibleSlideToRight(); 
+
+function right() {
+    drawVisibleSlideToRight(); 
+    let slides2 = document.querySelectorAll('.slide');
+    let offset2 = 0;
+
+    for (let i = 0; i < slides2.length; i++) {
+      slides2[i].style.left = offset2*(-1020) +1020 +'px';
+      offset2++;
+    }
+
+    setTimeout(function() {
+      slides2[0].remove()
+      drawVisibleSlideToRight();
+    }, 1000);
+}
+
+document.querySelector('.container__arrow_left').onclick = left;
+document.querySelector('.container__arrow_right').onclick = right;
+
+  
 
 //burger-menu
 
@@ -108,8 +191,7 @@ const removeSelectedMenu = () => {
 }
 
 const selectClickedMenu = (clickedMenu) => {
-  //clickedTag.classList.remove('tag_bordered');
-  clickedMenu.classList.add('active');
+     clickedMenu.classList.add('active');
 }
 
 addMenuClickHandler();
