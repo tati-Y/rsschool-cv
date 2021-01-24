b9d21b1f202d0cb9fa39f5c966f14b19ffcff42c
Tania Yufanova <ufanchik@gmail.com>
2021-01-24 19:48:45 +0300

    feat: add veriables

---
+++ calculator/main.js
@@ -0,0 +1,9 @@
+let btnNumber = document.querySelectorAll('.btn_number'),
+btnOperation = document.querySelectorAll('.btn_operation'),
+btnClear = document.getElementById('clear'),
+btnDot = document.getElementById('dot'),
+btnResult = document.getElementById('result'),
+btnDelete = document.getElementById('delete'),
+displayNum = document.querySelector('.display__numbers'),
+displayFormula = document.querySelector('.display__formula');
+
