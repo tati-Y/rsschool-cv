cad4f33e899326a56e96272ee9bce5fea24ca395
Tania Yufanova <ufanchik@gmail.com>
2021-02-21 23:28:58 +0300

    feat: add variabler css, js, media

---
+++ css_variables_and_js/main.js
@@ -0,0 +1,41 @@
+let buttonBox = document.querySelector(".buttons"),
+button = document.querySelectorAll(".button"),
+inputs = document.querySelectorAll("input"),
+inputDiv = document.querySelectorAll(".inputs div");
+
+function getDataFromBtn() {
+  buttonBox.addEventListener('click', (e) => {           
+      let prop = e.target.getAttribute("data-prop");
+      showProp(prop);        
+  });
+} 
+getDataFromBtn();
+
+function showProp(p) {
+  let key = document.querySelector(`div[data-prop="${p}"]`);
+  if (key.classList.contains('shown')) {
+    return;
+  } else {
+    removeSelectedBtn();
+    //tag.classList.add('hidden');
+    key.classList.add('shown');  
+  }   
+}
+
+function removeSelectedBtn() {     
+  inputDiv.forEach(tag => {
+     tag.classList.remove('shown'); 
+     tag.classList.add('hidden');            
+   });
+} 
+
+
+
+
+inputs.forEach(input => input.addEventListener("change", inputUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", inputUpdate));
+
+function inputUpdate() {
+    var suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
